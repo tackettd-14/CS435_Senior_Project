cb6c559863e3e0e16cdec8b8624a5d104e166101
cheriearia <viloetana@gmail.com>
2026-04-24 18:53:06 -0500

    Added query to join non-profits table with donations

--- pages/php/get_donations.php
+++ pages/php/get_donations.php
@@ -18,3 +18,26 @@ if ($mysqli->connect_error) {
     exit;
 }
 $mysqli->set_charset('utf8mb4');
+
+// Query to join donations and non-profit tables
+$sql = "
+    SELECT
+        d.List_id AS id,
+        np.Name AS name,
+        np.Street_address AS address,
+        np.City AS city,
+        np.State AS state,
+        np.Zip AS zip
+        np.Email AS email,
+        np.Area_code AS area_code,
+        np.Phone_number AS phone,
+        np.Website AS website,
+        np.Latitude AS latitude,
+        np.Longitude AS longitude,
+        np.Last_updated AS last_updated
+        np.Description AS description
+        dc.CName AS category
+    FROM Donation_Lists d
+    INNER JOIN Non_Profits np ON d.Nonprofit_id = np.Nonprofit_id
+    INNER JOIN Donation_Categories dc ON d.Donation_Category = dc.DCategory_id
+    ORDER BY np.Name ASC";
