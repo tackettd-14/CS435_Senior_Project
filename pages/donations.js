64056d7954a68a52c89d1dc9e2a18cbc587191c7
cheriearia <viloetana@gmail.com>
2026-04-26 17:42:42 -0500

    Made Donations its own separate JavaScript file

---
+++ pages/donations.js
@@ -0,0 +1,62 @@
+// Category config
+
+const cat_colors = {
+    "food": "lightgreen",
+    "household": "orange",
+    "clothing":  "slateblue",
+    "personal care": "purple",
+    "baby items": "lightblue",
+    "toys": "pink",
+    "bedding": "saddlebrown"
+};
+
+const cat_labels = {
+    "food": "food",
+    "household": "household",
+    "clothing": "clothing",
+    "personal care": "personal",
+    "baby items": "baby",
+    "toys": "toy",
+    "bedding": "bedding"
+};
+
+// Map state
+let DONATIONS = [];
+let activeFilter = "all";
+let markers = {};
+let map;
+
+// Map
+
+// Zoom config
+const config = {
+    minZoom: 7,
+    maxZoom: 18,
+};
+
+// Magnification with which the map will start
+const zoom = 10;
+const lat = 41.97787;
+const lng = -91.66562;
+
+map = L.map("map", config).setView([lat, lng], zoom);
+
+// Used to load and display tile layers on the map
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+
+function applyFilters() {
+
+}
+
+
+
+// Boot
+document.addEventListener("DOMContentLoaded", () => {
+    loadDonations();
+
+    document.getElementById("searchInput").addEventListener("input", applyFilters);
+});
+        
